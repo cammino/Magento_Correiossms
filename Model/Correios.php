3c34943aea948cb027e6239334095a75de5ff58f
Marcio Junior <marciofmjr@hotmail.com>
2019-02-12 10:57:15 -0200

    Remove return true fixo usado para teste

--- Model/Correios.php
+++ Model/Correios.php
@@ -1,6 +1,55 @@
 <?php
 class Cammino_Correiossms_Model_Correios {
 
+    public function syncOrdersToCorreios() {
+        try {
+            // Instacia helper
+            $helper = Mage::helper("correiossms");
+            
+            // Verifica se o módulo está ativo
+            if($helper->isModuleActive()) {
+
+                // Pega todos os pedidos a partir da data informada na config
+                foreach($this->getOrders() as $order) {
+
+                    // Pega a flag no banco para ver se o pedido já foi cadastrado
+                    $orderCorreioFlag = $order->getCorreiossms();
+
+                    // Pega o status do pedido para saber se já foi criada uma entrega
+                    $status = $order->getStatus();
+
+                    /* Se o pedido estiver com o status completo ('entregue') 
+                    E não tiver uma flag de que o pedido já foi sincronizado no banco */
+                    if(empty($orderCorreioFlag) && $status == "complete") {
+
+                        // Pega o código de rastreio do pedido
+                        $tracking = $this->getTrackingCode($order);
+
+                        // Pega o celular do cliente
+                        $cellphone = $this->getCustomerCellphone($order->getCustomerId());
+                        
+                        // Se existir um código de rastreio para o pedido e o cliente tiver um celular valido
+                        if(!empty($tracking) && $cellphone != false) { 
+
+                            // Registra o celular do cliente no correio
+                            $status = $this->registerInCorreios($tracking, $cellphone);
+
+                            /* Se conseguiu cadastrar o celular do cliente no correios,
+                            atualiza a flag para o pedido no banco, indicando que esse pedido ja foi
+                            cadastrado e não precisa ser sincronizado novamente */
+                            if($status) {
+                                $order->setCorreiossms("1")->save();
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception $e) {
+            Mage::log("Erro ao sincronizar os pedidos no módulo Correiossms");
+            Mage::log($e->getMessage());
+        }
+    }
+
     public function getOrders() {
         $helper = Mage::helper("correiossms");
         $date = $helper->getSyncFromDate();
@@ -56,7 +105,6 @@ class Cammino_Correiossms_Model_Correios {
     }
 
     public function registerInCorreios($tracking, $cellphone) {
-        return true;
         try {
             $helper = Mage::helper("correiossms");
             $cellphone = Mage::helper("correiossms")->formatCellphoneToCorreios($cellphone);
